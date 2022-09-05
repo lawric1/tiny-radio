120f72a6b154d1063dc621b0df445c4119313a66
lawric1 <67882089+lawric1@users.noreply.github.com>
2022-09-05 01:11:31 -0300

    Change audio embed system.
    Add previous/next buttom logic.
    
    Add system to easily fetch all the data needed from stream IDs.

--- tiny-radio/src/main.ts
+++ tiny-radio/src/main.ts
@@ -1,23 +1,65 @@
 import './style.css'
-// import typescriptLogo from './typescript.svg'
-// import { setupCounter } from './counter'
 
-// document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="/vite.svg" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://www.typescriptlang.org/" target="_blank">
-//       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-//     </a>
-//     <h1>Vite + TypeScript</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite and TypeScript logos to learn more
-//     </p>
-//   </div>
-// `
+let audio = <HTMLIFrameElement>document.getElementById("audio");
 
-// setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+let title = <HTMLParagraphElement>document.getElementById("title");
+let backdrop = <HTMLImageElement>document.getElementById("backdrop");
+let bannerImage = <HTMLImageElement>document.getElementById("bannerImage");
+
+let previousButton = <HTMLImageElement>document.getElementById("previous");
+let nextButton = <HTMLImageElement>document.getElementById("next");
+let playButton = <HTMLImageElement>document.getElementById("play");
+
+
+let stationDB = ["6uE8SJFBjZc", "HW_vf_aDWws", "jfKfPfyJRdk"]
+
+
+function toggleSound() {
+    // State can be "0" (unmuted) or "1" (muted)
+    let state = audio.src.slice(-1);
+    let url = audio.src.slice(0, -1);
+
+    if (state === "0") {
+        audio.src = url + "1"
+    } else {
+        audio.src = url + "0"
+    }
+}
+
+function previousStation() {
+    let videoID: string = audio.src.slice(30, -18)
+    let index: number = (stationDB.indexOf(videoID) - 1) % stationDB.length;
+
+    setData(stationDB[index]);
+}
+
+function nextStation() {
+    let videoID: string = audio.src.slice(30, -18)
+    let index: number = (stationDB.indexOf(videoID) + 1) % stationDB.length;
+
+    setData(stationDB[index]);
+}
+
+
+async function setData(videoID: string) {
+    let url = "https://www.youtube.com/embed/" + videoID + "?autoplay=1&mute=0";
+    let thumbnailURL = "https://img.youtube.com/vi/" + videoID + "/maxresdefault.jpg";
+
+    let data = await fetch("https://noembed.com/embed?url=" + url)
+        .then(reponse => reponse.json());
+
+    title.innerHTML = data.title;
+    backdrop.src = thumbnailURL;
+    bannerImage.src = thumbnailURL;
+    audio.src = url;
+}
+
+
+window.addEventListener("load", () => {
+    let randomIndex = Math.floor(Math.random() * stationDB.length);
+    setData(stationDB[randomIndex]);
+});
+
+playButton.addEventListener('click', toggleSound);
+previousButton.addEventListener('click', previousStation);
+nextButton.addEventListener('click', nextStation);
